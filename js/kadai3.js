eb423ea631698e18cd4d3b88ec1ed243f6e0d326
shiryu99 <68994033+shiryu99@users.noreply.github.com>
2020-07-30 23:16:40 +0900

    Add files via upload

---
+++ js/kadai3.js
@@ -0,0 +1,48 @@
+$('#jquery-h2A').click(function(){
+    
+    $('#jquery-p1').text('印象深かった事');
+    
+    $('#jquery-p1').fadeToggle();
+    
+    $('#jquery-p4').fadeToggle();
+});
+
+$('#jquery-h2B').click(function(){
+    
+    $('#jquery-p2').text('印象深かった事');
+    
+    $('#jquery-p2').fadeToggle();
+    
+    $('#jquery-p5').fadeToggle();
+    
+});
+
+$('#jquery-h2C').click(function(){
+    
+    $('#jquery-p3').text('印象深かった事');
+    
+    $('#jquery-p3').fadeToggle();
+    
+    $('#jquery-p6').fadeToggle();
+});
+
+
+
+
+$('#jquery-p1').click(function(){
+    
+    $('#jquery-p4').text('中２の夏に大阪から東京に引っ越したのが自分にとって大きな出来事でした. 友人関係も新しく作っていかなければならないのはもちろんのこと, 関西と関東でそもそもの価値観が違うというのも大変でした. ');
+    
+});
+
+$('#jquery-p2').click(function(){
+    
+    $('#jquery-p5').text('今の自分の核と言ってもいいほどの「自分」が形成されたのがこの時期でした. コミュニケーション能力は元々あった方なのですが, それでも足りない程周りのコミュニケーション能力が高く, 自分のコミュニケーション能力の向上や話の幅が広がったりと, １番楽しい時間でした. ');
+    
+});
+
+$('#jquery-p3').click(function(){
+    
+    $('#jquery-p6').text('大学でであった友達は皆大人びていて, 自身の考えや核をしっかり持っているというのが印象的でした. 高校生活が楽しかったのもあり, 楽しいが第一優先だった自分にとって刺激的で, 転換期ともなる良い出会いでした. ');
+    
+});
